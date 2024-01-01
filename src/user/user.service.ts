e3d592f5e2bbe30e92658835cb40f5fb9c40c78e
Ed Salisbury <ed.salisbury@gmail.com>
2024-01-01 06:44:32 -0700

    Replaced TypeORM with Prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from "@nestjs/common";
-import { User } from "./user.entity";
-import { AppDataSource } from "../data-source";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
   async create(createUserDto: CreateUserDto) {
-    const userRepository = AppDataSource.getRepository(User);
-    const user = new User();
-    user.name = createUserDto.name;
-    await userRepository.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    return this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+      },
+    });
   }
 }
